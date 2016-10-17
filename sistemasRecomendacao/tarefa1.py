24fbb98b013be272551b3206e406b4ac103340f4
Richard Kevin <richardkevin@gmail.com>
2016-10-17 00:50:07 -0200

    refactoring

--- sistemasRecomendacao/tarefa1.py
+++ sistemasRecomendacao/tarefa1.py
@@ -26,10 +26,28 @@ jsonfile.write('\n')
 jsonfile.close()
 
 
+def userInput():
+    print "Entre apenas com numero entre 1 e 10"
+    num = input()
+    return num if num in range(1, 11) else userInput()
+
+
 def checkIfUserRatedItem(user, item):
     if users[user][item] != "?":
         return True
     return False
+
+
+def getItemRated(user, item):
+    return users[user][item]
+
+
+def usersThatRatedItemY(item):
+    count = 0
+    for user in users:
+        if checkIfUserRatedItem(user, item):
+            count += 1
+            return count
 
 
 def countUserRates(user):
@@ -38,23 +56,17 @@ def countUserRates(user):
         for i in users[user]:
             if users[user][i] == "?":
                 count += 1
-        print "o usuario {0} avaliou {1} itens".format(user, 10-count)
+        return 10-count
 
 
 def howManyUsersRatedItem(item):
     count = 0
+    user_with_same_rates = []
     for user in users:
         if users[user][item] != "?":
             count += 1
-    print "{0} usuarios avaliaram o item {1}".format(count, item)
-
-
-def userInput():
-    print "Informe o Usuario desejado: "
-    user = input()
-    print "Informe o Item desejado: "
-    item = input()
-    return user, item
+            user_with_same_rates.append(user)
+    return (count, user_with_same_rates)
 
 
 def checkEqualUserRates(user):
@@ -66,24 +78,45 @@ def checkEqualUserRates(user):
     return count
 
 
-def checkUsersThatRatedItemY(item):
-    count = 0
-    for user in users:
-        if checkIfUserRatedItem(user, item):
-            count += 1
-    return count
+def predBasedUsers(user, item):
+    users_rated = howManyUsersRatedItem(item)
+    rates = {}
+    diff_user = {}
+    for user_r in users_rated[1]:
+        rates[user_r] = users[user_r]
+    for r in rates:
+        diff_rates = {}
+        for item_r in rates[r]:
+            diff_rates[item_r] = (users[user][item_r], rates[r][item_r])
+        diff_user[r] = diff_rates
+    # comparar e ver similaridades
 
 
-user, item = userInput()
 
+def predBasedItems():
+    pass
+
+
+user, item = "User 9", "Item 10"
+# predBasedUsers(user, item)
+# print "Qual usuario deseja selecionar?"
+# user = "User " + str(userInput())
+# print "Qual item deseja visualizar?"
+# item = "Item " + str(userInput())
 if checkIfUserRatedItem(user, item):
-    print "Rxy"
+    print "O '{}' avaliou o '{}' com: {}".format(
+        user,
+        item,
+        getItemRated(user, item)
+    )
 else:
-    print "Quantas pessoas avaliaram o item {}?".format(item)
-    howManyUsersRatedItem(item)
+    print "Quantas pessoas avaliaram o {}?".format(item)
+    print "{} usuarios\n".format(howManyUsersRatedItem(item)[0])
     print "Quantos itens o {} avaliou?".format(user)
-    countUserRates(user)
-    print "Numero de usuarios que avaliou os mesmos itens que o {}".format(user)
-    print checkEqualUserRates(user)
-    print "Numero de usuarios que avaliaram o {}".format(item)
-    print checkUsersThatRatedItemY(item)
+    print "{} itens\n".format(countUserRates(user))
+    print "Quantos usuarios avaliaram os mesmos itens que o {}?".format(user)
+    print "{} usuarios \n".format(checkEqualUserRates(user))
+    print "Quantos usuarios avaliaram o {}?".format(item)
+    print "{} usuarios\n".format(usersThatRatedItemY(item))
+    print "pred(rx,y) baseado em usuarios"
+    print predBasedUsers(user, item)
